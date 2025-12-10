752eb552767fae07e747e13ee61e2898fd761bc4
RoiRendal <23-16160@g.batstate-u.edu.ph>
2025-12-10 10:16:46 +0800

    feat: render the App.jsx and its components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*
+      App.jsx imports the components and returns the Home page.
+      The CatDetail page only loads when the URL changes (depending on which cat, that is).
+      
+      main.jsx imports App.jsx and renders the file with the 'root' id to display what's in the App.jsx file.
+      index.html is our empty stage (the one with the element containing the 'root' id) that gets filled in by main.jsx.
+      BrowserRouter element wraps around the App element so that everything loads somehow.
+    */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
